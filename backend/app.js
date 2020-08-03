e9f2de03f226ebf79d4db3ff44b73803a91d4016
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2020-08-03 17:24:18 +0300

    Update №1

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require('express')
-
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Book Store Application'))
-app.listen(3000, () => console.log(`App has been started on port: ${port}.`))
+app.get('/', (request, response) => response.send('Book Store Application'))
+app.listen(port, () => console.log(`App has been started on port: ${port}.`))
